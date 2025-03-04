7d76d57a71f07ba7cdb10fcf8356022781530a6b
AvelP <nikkolayssobolev@gmail.com>
2025-03-04 15:24:39 +0300

    компонент

--- local/deal.list/class.php
+++ local/deal.list/class.php
@@ -3,6 +3,7 @@ namespace Local\Components;
 
 use \Bitrix\Main\Loader;
 use \Bitrix\Main\Engine\ComponentController;
+use \Bitrix\Main\Data\Cache;
 
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
@@ -10,6 +11,8 @@ class DealList extends \CBitrixComponent
 {
     public function executeComponent()
     {
+        Cache::clearCache(true);
+
         if (!Loader::includeModule("crm")) {
             ShowError("Модуль CRM не установлен!");
             return;
