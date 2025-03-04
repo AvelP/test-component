99b009e174af8e77ea2f2b8825bbe756d31b69c8
AvelP <nikkolayssobolev@gmail.com>
2025-03-04 15:26:25 +0300

    компонент

--- local/deal.list/templates/.default/template.php
+++ local/deal.list/templates/.default/template.php
@@ -1,4 +1,4 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 
 <div id="app"></div>
-<script src="<?= $templateFolder ?>/script.js"></script>
+<script src="<?= $templateFolder ?>/script.js?<?php echo time(); ?>"></script>
