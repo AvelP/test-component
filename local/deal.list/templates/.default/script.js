a8a3cc683931496d48beb7b7886446570d4e5d70
AvelP <nikkolayssobolev@gmail.com>
2025-03-04 18:17:15 +0300

    компонент

--- local/deal.list/templates/.default/script.js
+++ local/deal.list/templates/.default/script.js
@@ -4,28 +4,62 @@ BX.Vue3.createApp({
             deals: [],
             loading: true,
             error: null,
+            currentPage: 1,  // Текущая страница
+            itemsPerPage: 5, // Сделок на страницу
+            totalPages: 1,   // Всего страниц
+            sortKey: "TITLE", // Поле для сортировки
+            sortOrder: "asc" // Направление сортировки
         };
     },
+    computed: {
+        paginatedDeals() {
+            const start = (this.currentPage - 1) * this.itemsPerPage;
+            return this.deals.slice(start, start + this.itemsPerPage);
+        }
+    },
     mounted() {
-        BX.ajax({
-            url: "/local/components/local/deal.list/ajax.php?action=getDeals",
-            method: "GET",
-            dataType: "json",
-            onsuccess: (response) => { // Используем стрелочную функцию
-                console.log(response);
-                if (response.status === "success") {
-                    this.deals = response.data;
-                } else {
+        this.fetchDeals();
+    },
+    methods: {
+        fetchDeals() {
+            this.loading = true;
+            BX.ajax({
+                url: "/local/components/local/deal.list/ajax.php",
+                method: "GET",
+                dataType: "json",
+                data: {
+                    action: "getDeals",
+                    sortKey: this.sortKey,
+                    sortOrder: this.sortOrder,
+                    currentPage: this.currentPage,
+                    itemsPerPage: this.itemsPerPage
+                },
+                onsuccess: (response) => {
+                    if (response.status === "success") {
+                        this.deals = response.data.deals;
+                        this.totalPages = Math.ceil(response.data.totalCount / this.itemsPerPage);
+                    } else {
+                        this.error = "Ошибка загрузки данных!";
+                    }
+                    this.loading = false;
+                },
+                onfailure: (error) => {
+                    console.error("Ошибка загрузки данных!", error);
                     this.error = "Ошибка загрузки данных!";
-                }
-                this.loading = false;
-            },
-            onfailure: (error) => {
-                console.error("Ошибка загрузки данных!", error);
-                this.error = "Ошибка загрузки данных!";
-                this.loading = false;
-            },
-        });
+                    this.loading = false;
+                },
+            });
+        },
+        changePage(page) {
+            if (page >= 1 && page <= this.totalPages) {
+                this.currentPage = page;
+                this.fetchDeals(); // Загружаем новые данные
+            }
+        },
+        sortDeals() {
+            this.sortOrder = this.sortOrder === "asc" ? "desc" : "asc";
+            this.fetchDeals();
+        }
     },
     template: `
         <div>
@@ -34,17 +68,17 @@ BX.Vue3.createApp({
             <div v-if="error">{{ error }}</div>
             <table v-if="!loading && !error">
                 <tr>
-                    <th @click="sortDeals">Название ▲</th>
+                    <th @click="sortDeals">Название {{ sortOrder === 'asc' ? '▲' : '▼' }}</th>
                 </tr>
-                <tr v-for="deal in deals" :key="deal.ID">
+                <tr v-for="deal in paginatedDeals" :key="deal.ID">
                     <td>{{ deal.TITLE }}</td>
                 </tr>
             </table>
+            <div v-if="totalPages > 1">
+                <button @click="changePage(currentPage - 1)" :disabled="currentPage === 1">Назад</button>
+                <span>Страница {{ currentPage }} из {{ totalPages }}</span>
+                <button @click="changePage(currentPage + 1)" :disabled="currentPage === totalPages">Вперед</button>
+            </div>
         </div>
-    `,
-    methods: {
-        sortDeals() {
-            this.deals.sort((a, b) => a.TITLE.localeCompare(b.TITLE));
-        }
-    }
+    `
 }).mount("#app");
